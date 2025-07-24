39fe5b1824f69baf51c01be5ce134575b146729c
herrgottsackzement <dynado@fc4a.de>
2025-07-25 00:03:45 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>DynaDo</h1>
+      <p>Willkommen bei deiner dynamischen ToDo-App!</p>
+    </div>
+  );
+}
+
+export default App;
